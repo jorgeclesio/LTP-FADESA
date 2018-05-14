77e6dc3cdcc7b7b7cbc2a8e2a047e6490849342b
Jorge Clésio <jorgepebas@gmail.com>
2018-05-14 18:20:05 -0300

    Add files via upload

---
+++ AULA9-PHP/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+	<title>Aula9 - Blocos</title>
+	<link href="estilo.css" rel="stylesheet">
+</head>
+<body>
+<div id="principal">
+	<header>Cabeçalho</header>
+	<nav>Menu
+	<a href="#section">Box 1</a>
+<?php  ?>
+	</nav>
+<section id="section">
+	<div class="conteudo cor_verde texto_rosa">Conteúdo 1</div>
+	<div class="conteudo cor_azul texto_branco">Conteúdo 2</div>
+</section>
+
+	<aside>Lateral</aside>
+	
+	<footer >Rodapé</footer>
+</div>	
+</body>
+</html>
---
+++ AULA9-PHP/sintax.php
@@ -0,0 +1,9 @@
+<title>Sintaxe</title>
+
+
+<?php
+$nome ='Mayara';
+$idade="30" . "anos";
+$valor = 4+4;
+echo $valor;
+?>
